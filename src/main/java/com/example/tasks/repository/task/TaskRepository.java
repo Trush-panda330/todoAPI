2ff7ab86a7508dcc5a2615a5322ff1723a6873f1
uetak330 <uetak330@gmail.com>
2024-12-04 18:26:16 +0900

    Selectのクエリを記述

--- src/main/java/com/example/tasks/repository/task/TaskRepository.java
+++ src/main/java/com/example/tasks/repository/task/TaskRepository.java
@@ -1,14 +1,15 @@
 package com.example.tasks.repository.task;
 
-import com.example.tasks.domain.model.Task;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface TaskRepository {
 
-
-    List<TaskRecord> selectList(int limit, long offset);
+    @Select("SELECT id, title, details, created_at, updated_at, status, is_deleted " +
+            "FROM tasks WHERE is_deleted = 0 ")
+    List<TaskRecord> selectList();
 
 }
